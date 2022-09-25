ef1534d8a759f78b132d1acabbbc6e3f0fa18190
Michail Petrov <Gismanus@gmail.com>
2022-09-26 02:05:50 +0300

    done exercise

--- js/ladder.js
+++ js/ladder.js
@@ -2,23 +2,26 @@ let ladder = {
     step: 0,
 
     up() {
-        this.step++
+        this.step++;
+        return this;
     },
 
     down() {
         this.step--;
+        return this;
     },
 
     showStep: function () {
         console.log(this.step);
+        return this;
     }
 }
 
-ladder.up();
-ladder.up();
-ladder.down();
-ladder.showStep(); // 1
-ladder.down();
-ladder.showStep(); // 0
+// ladder.up();
+// ladder.up();
+// ladder.down();
+// ladder.showStep(); // 1
+// ladder.down();
+// ladder.showStep(); // 0
 
-//ladder.up().up().down().showStep().down().showStep(); // shows 1, then 0
+ladder.up().up().down().showStep().down().showStep(); // shows 1, then 0
